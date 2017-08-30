bc5e9df8ed6a380c1dff27ebf6bea5daf667bc85
Andrés Souto <kai670@gmail.com>
2017-08-30 09:39:44 +0200

    Wait until the video is ready before taking a photo or grabing a frame

--- src/imagecapture.js
+++ src/imagecapture.js
@@ -40,6 +40,9 @@ if (typeof ImageCapture === 'undefined') {
       // MediaStream constructor not available until Chrome 55 - https://www.chromestatus.com/feature/5912172546752512
       this._previewStream = new MediaStream([videoStreamTrack]);
       this.videoElement = document.createElement('video');
+      this.videoElementPlaying = new Promise(resolve => {
+        this.videoElement.addEventListener('playing', resolve);
+      });
       this.videoElement.src = URL.createObjectURL(this._previewStream);
       this.videoElement.muted = true;
       this.videoElement.play();  // required by Firefox
@@ -107,25 +110,19 @@ if (typeof ImageCapture === 'undefined') {
       return new Promise(function executorTP(resolve, reject) {
         // `If the readyState of the MediaStreamTrack provided in the constructor is not live,
         // return a promise rejected with a new DOMException whose name is "InvalidStateError".`
-        if (self._videoStreamTrack.readyState === 'live') {
-          // -- however, checking for `live` alone doesn't guarantee the video is ready
-          if (self.videoElement.videoWidth) {
-            try {
-              self.canvasElement.width = self.videoElement.videoWidth;
-              self.canvasElement.height = self.videoElement.videoHeight;
-              self.canvas2dContext.drawImage(self.videoElement, 0, 0);
-              self.canvasElement.toBlob(blob => {
-                resolve(blob);
-              });
-            } catch (error) {
-              reject(new DOMException('UnknownError'));
-            }
-          } else {
+        if (self._videoStreamTrack.readyState !== 'live') {
+          return reject(new DOMException('InvalidStateError'));
+        }
+        self.videoElementPlaying.then(() => {
+          try {
+            self.canvasElement.width = self.videoElement.videoWidth;
+            self.canvasElement.height = self.videoElement.videoHeight;
+            self.canvas2dContext.drawImage(self.videoElement, 0, 0);
+            self.canvasElement.toBlob(resolve);
+          } catch (error) {
             reject(new DOMException('UnknownError'));
           }
-        } else {
-          reject(new DOMException('InvalidStateError'));
-        }
+        });
       });
     }
 
@@ -138,27 +135,23 @@ if (typeof ImageCapture === 'undefined') {
     grabFrame() {
       let self = this;
       return new Promise(function executorGF(resolve, reject) {
-        if (self._videoStreamTrack.readyState === 'live') {
-          if (self.videoElement.videoWidth) {
-            try {
-              // videoWidth is available after videoElement.onloadedmetadata fires
-              self.canvasElement.width = self.videoElement.videoWidth;
-              self.canvasElement.height = self.videoElement.videoHeight;
-              // The video has an image after videoElement.oncanplay triggers
-              self.canvas2dContext.drawImage(self.videoElement, 0, 0);
-              // TODO polyfill https://developer.mozilla.org/en-US/docs/Web/API/ImageBitmapFactories/createImageBitmap for IE
-              resolve(window.createImageBitmap(self.canvasElement));
-            } catch (error) {
-              reject(new DOMException('UnknownError'));
-            }
-          } else {
+        // `If the readyState of the MediaStreamTrack provided in the constructor is not live,
+        // return a promise rejected with a new DOMException whose name is "InvalidStateError".`
+        if (self._videoStreamTrack.readyState !== 'live') {
+          return reject(new DOMException('InvalidStateError'));
+        }
+        self.videoElementPlaying.then(() => {
+          try {
+            self.canvasElement.width = self.videoElement.videoWidth;
+            self.canvasElement.height = self.videoElement.videoHeight;
+            self.canvas2dContext.drawImage(self.videoElement, 0, 0);
+            // TODO polyfill https://developer.mozilla.org/en-US/docs/Web/API/ImageBitmapFactories/createImageBitmap for IE
+            resolve(window.createImageBitmap(self.canvasElement));
+          } catch (error) {
             reject(new DOMException('UnknownError'));
           }
-        } else {
-          reject(new DOMException('InvalidStateError'));
-        }
+        });
       });
     }
-
   };
 }
